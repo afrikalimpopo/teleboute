97353f3b0c934f5081212291f1abab3670033f56
timofiimysov <tymofii.mysov@gmail.com>
2025-03-19 18:55:21 +0100

    JRTB-2: added stub telegram bot to the project

---
+++ src/test/java/com/com/github/afrikalimpopo/pet_teleboute/jrtb/TelebouteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.com.github.afrikalimpopo.pet_teleboute.jrtb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelebouteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
